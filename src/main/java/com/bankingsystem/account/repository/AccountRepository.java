6afb3cef23ed7c295861d3cf701a11976221c56c
Gopi <mr.gopi0004@gmail.com>
2025-12-27 19:19:18 +0530

    feat: added transaction details for admin dashboard

--- src/main/java/com/bankingsystem/account/repository/AccountRepository.java
+++ src/main/java/com/bankingsystem/account/repository/AccountRepository.java
@@ -2,14 +2,40 @@ package com.bankingsystem.account.repository;
 
 import com.bankingsystem.account.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account,Long> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
     Optional<Account> findByAccountNumber(String accountNumber);
 
     Optional<Account> findByAccountId(Long accountId);
+
+
+    @Query("SELECT COUNT(a) FROM Account a")
+    long countTotalAccounts();
+
+    @Query("SELECT COUNT(a) FROM Account a WHERE a.accountType = 'SAVINGS'")
+    long countSavingsAccounts();
+
+    @Query("SELECT COUNT(a) FROM Account a WHERE a.accountType = 'CURRENT'")
+    long countCurrentAccounts();
+
+    @Query("SELECT COUNT(a) FROM Account a WHERE a.status = 'ACTIVE'")
+    long countActiveAccounts();
+
+    @Query("SELECT COUNT(a) FROM Account a WHERE a.status = 'BLOCKED'")
+    long countBlockedAccounts();
+
+    @Query("SELECT COALESCE(SUM(a.balance), 0) FROM Account a WHERE a.status = 'ACTIVE'")
+    double totalBalance();
+
+
+
+
 }
--- src/main/java/com/bankingsystem/transaction/repository/TransactionRepository.java
+++ src/main/java/com/bankingsystem/transaction/repository/TransactionRepository.java
@@ -12,38 +12,51 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
 
-    Page<Transaction> findAllByAccount(Account accountId,
-                                       Pageable pageable);
+    Page<Transaction> findAllByAccount(Account accountId,Pageable pageable);
 
 
-    Page<Transaction> findAllByAccountAndType(
-            Account account,
-            TransactionType type,
-            Pageable pageable
+    Page<Transaction> findAllByAccountAndType(Account account, TransactionType type, Pageable pageable
     );
 
-    Page<Transaction> findAllByTimestampBetween(
-            LocalDateTime start,
-            LocalDateTime end,
-            Pageable pageable
+    Page<Transaction> findAllByTimestampBetween(LocalDateTime start, LocalDateTime end, Pageable pageable
     );
 
-      @Query("""
-                  SELECT t FROM Transaction t
-                  WHERE t.timestamp BETWEEN :start AND :end
-                  AND (:type IS NULL OR t.type = :type)
-              """)
-    Page<Transaction> `findAllForManager`(
+    @Query("""
+                SELECT t FROM Transaction t
+                WHERE t.timestamp BETWEEN :start AND :end
+                AND (:type IS NULL OR t.type = :type)
+            """)
+    Page<Transaction> findAllForManager(
             LocalDateTime start,
             LocalDateTime end,
             TransactionType type,
             Pageable pageable
     );
 
+    @Query("""
+            SELECT COALESCE(SUM(t.amount), 0)
+            FROM Transaction t
+            WHERE t.type = 'DEPOSIT'
+            AND t.timestamp BETWEEN :start AND :end
+            """)
+    double todayDeposits(LocalDateTime start, LocalDateTime end);
 
+    @Query("""
+            SELECT COALESCE(SUM(t.amount), 0)
+            FROM Transaction t
+            WHERE t.type = 'WITHDRAW'
+            AND t.timestamp BETWEEN :start AND :end
+            """)
+    double todayWithdrawals(LocalDateTime start, LocalDateTime end);
 
+    @Query("""
+            SELECT COUNT(t)
+            FROM Transaction t
+            WHERE t.timestamp BETWEEN :start AND :end
+            """)
+    long todayTransactions(LocalDateTime start, LocalDateTime end);
 
 }
