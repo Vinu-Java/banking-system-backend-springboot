77c9a6776d732c70c9afd5444bb90ff3570089b6
Vinu-AD <vinu882368@gmail.com>
2025-12-29 11:12:01 +0530

    feat: added Dockerfile

--- src/main/java/com/bankingsystem/config/CorsConfig.java
+++ src/main/java/com/bankingsystem/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://digital-banking-system-application.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
